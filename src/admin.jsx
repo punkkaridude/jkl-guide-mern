c4beabb504486aef3785fe65d08f59c17be18ee9
dahliacadavre <M1921@student.jamk.fi>
2020-04-17 01:12:17 +0300

    admin-sivua nätitetty

--- src/admin.jsx
+++ src/admin.jsx
@@ -44,15 +44,22 @@ export default class Admin extends React.Component {
         
         
         return services.map((service, index) => (
-            <div className="card">
-                <div className="card-header d-flex justify-content-between"  key={index}>
-                    <h2 className="pt-0 m-0">{service.name}</h2>
-                </div>
-                <div className="card-body">
-                     <p>{service.address}</p>
-                </div>
-                
-                
+
+            <div key={index} className="d-flex justify-content-around flex-wrap">
+            <table class="table">
+                <thead class="thead-light">
+                    <tr>
+                    <th scope="col">Service Name</th>
+                    <th scope="col">Service Address</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td scope="row">{service.name}</td>
+                    <td scope="row">{service.address}</td>
+                    </tr>
+                </tbody>
+            </table>
             </div>
         ));
     };
@@ -60,28 +67,40 @@ export default class Admin extends React.Component {
     displayUsers = (users) => {
         if (!users.length) return null;
         return users.map((user, index) => (
-                <div className="card h-100 shadow d-flex flex-column" key={index}>
-                    <div className="card-header shadow">{user.username}</div>
-                    <div className="title card-header d-flex">
-                        <p className="col-8">{user.fullname}</p>
-                    </div>
-                </div>
+
+            <div key={index} className="justify-content-around flex-wrap">
+            <table class="table">
+                <thead class="thead-light">
+                    <tr>
+                    <th scope="col">Username</th>
+                    <th scope="col">Full name</th>
+                    <th scope="col">Email address</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td>{user.username}</td>
+                    <td>{user.fullname}</td>
+                    <td>{user.email}</td>
+                    </tr>
+                </tbody>
+            </table>
+            </div>
         ));
     };
 
     render(){
         return(
-            <div id="forumWrapper" className="d-flex justify-content-around flex-wrap">
-            <div className="col-lg-8 pr-lg-0 pl-xl-5 pl-lg-3 pt-3 pt-md-0">
-                <div id ="fContainer" className="card h-100 justify-content-start">
-                    {this.displayServices(this.state.services)}    
-                </div>
-            </div>
-
+            <div id="adminWrapper" className="d-flex justify-content-around flex-wrap">
                 <div>
-                    {this.displayUsers(this.state.users)}
-                </div>
-            </div>
+                    <h2>Added services:</h2>
+                    {this.displayServices(this.state.services)}    
+                </div>             
+                <div>
+                    <h2>Added users:</h2>
+                        {this.displayUsers(this.state.users)}
+                </div>  
+            </div>    
         );
     }
 }
